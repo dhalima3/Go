869a2e5d8ee403d91ab6f71b7917dc7be12eb8f3
dhalima3 <dhalima@gatech.edu>
2015-04-09 01:14:39 -0400

    Home layout

--- Go/app/src/main/java/com/example/daryl/go/MainActivity.java
+++ Go/app/src/main/java/com/example/daryl/go/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.daryl.go;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private GoogleMap googleMap;
+    private AutoCompleteTextView mSourceAutoComplete;
+    private AutoCompleteTextView mDestinationAutoComplete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+
+        mSourceAutoComplete = (AutoCompleteTextView) findViewById(R.id.pickUpEdit);
+        mDestinationAutoComplete = (AutoCompleteTextView) findViewById(R.id.dropEdit);
+        zoomMapCurrentLocation();
     }
 
 
@@ -35,5 +53,35 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void zoomMapCurrentLocation() {
+//      Get location from GPS if it's available
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+//        If Location wasn't found, find the next most accurate place
+        if (myLocation == null){
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+            String provider = locationManager.getBestProvider(criteria, true);
+            myLocation = locationManager.getLastKnownLocation(provider);
+        }
+
+        if (myLocation != null) {
+//          Animate camera to myLocation
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), 15));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()))
+                    .zoom(15)
+                    .bearing(90)
+                    .tilt(40)
+                    .build();
+
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
     }
 }
